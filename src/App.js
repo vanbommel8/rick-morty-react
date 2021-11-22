3e7d29579bf3d78f255b43d6d238fa77f53a4026
Daniele Mauro <danima82@gmail.com>
2021-11-22 15:40:01 +0100

    fix nav

--- src/App.js
+++ src/App.js
@@ -1,40 +1,23 @@
 import Characters from "./Characters";
 import Navigation from "./Navigation";
 import { useState } from "react";
-import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
 
+import React from "react";
 
 function App() {
-  
+
   const [page, setPage] = useState(1);
   const [maxPage, setMaxPage] = useState(0);
 
   return (
     <div className='App'>
-      <Router>
+
       <Navigation page={page} setPage={setPage} maxPage={maxPage} />
       <Characters page={page} setMaxPage={setMaxPage} />
       <Navigation page={page} setPage={setPage} maxPage={maxPage} />
 
-      <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/users">
-            <Users />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
 
-      </Router>
+
     </div>
   );
 }
